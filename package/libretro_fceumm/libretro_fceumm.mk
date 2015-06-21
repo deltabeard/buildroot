ccf67a408fe18ebaad52a35a763837251ed8921c
Mahyar Koshkouei <deltabeard@users.noreply.github.com>
2015-06-21 13:20:52 +0100

    Add libretro_fceumm This is slow on the Raspberry Pi. libretro_nestopia is recommended.
    Signed-off-by: Mahyar Koshkouei <deltabeard@users.noreply.github.com>

---
+++ package/libretro_fceumm/libretro_fceumm.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# FCEUMM
+#
+################################################################################
+LIBRETRO_FCEUMM_VERSION = 19054984ff26aefa6b405661565c95a5c11c56a4
+LIBRETRO_FCEUMM_SITE = $(call github,libretro,libretro-fceumm,$(LIBRETRO_FCEUMM_VERSION))
+
+
+define LIBRETRO_FCEUMM_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_FCEUMM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/fceumm_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/fceumm_libretro.so
+endef
+
+$(eval $(generic-package))
