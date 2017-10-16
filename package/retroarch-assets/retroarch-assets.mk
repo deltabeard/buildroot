82a6548e0af9981c8253264796fb736ff9f49d7a
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2017-10-16 20:57:10 +0100

    Point retroarch-assets to fork w/ XMB only option
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

--- package/retroarch-assets/retroarch-assets.mk
+++ package/retroarch-assets/retroarch-assets.mk
@@ -3,11 +3,15 @@
 # retroarch-assets
 #
 ################################################################################
-RETROARCH_ASSETS_VERSION = 3acffa41f215adcaa4b675055dbb4c8b19ccb74c
-RETROARCH_ASSETS_SITE = $(call github,libretro,retroarch-assets,$(RETROARCH_ASSETS_VERSION))
+RETROARCH_ASSETS_VERSION = 80dc7b60a5b5e7b976e005583d6c5569534e9869
+RETROARCH_ASSETS_SITE = $(call github,deltabeard,retroarch-assets,$(RETROARCH_ASSETS_VERSION))
 
 define RETROARCH_ASSETS_INSTALL_TARGET_CMDS
+ifeq($(BR2_PACKAGE_RETROARCH_ASSETS_XMB_ONLY),y)
+	$(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install-xmb-monochrome
+else
 	$(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
+endif
 endef
 
 $(eval $(generic-package))
