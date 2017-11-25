387b4f0042c204226e753a0193195dc5c7b47da8
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2017-11-25 22:06:17 +0000

    libretro-pcsx: fix package
    Fixed an issue whereby the package would be forced to compile for the
    Raspberry Pi 1 only.
    
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

--- package/libretro-pcsx/libretro-pcsx.mk
+++ package/libretro-pcsx/libretro-pcsx.mk
@@ -9,7 +9,7 @@ LIBRETRO_PCSX_SITE = $(call github,libretro,pcsx_rearmed,$(LIBRETRO_PCSX_VERSION
 define LIBRETRO_PCSX_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) \
 		   CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) \
-		   -f Makefile.libretro platform="rpi1"
+		   -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_PCSX_INSTALL_TARGET_CMDS
