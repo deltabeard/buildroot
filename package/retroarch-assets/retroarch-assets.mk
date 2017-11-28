6098b82dff535b1d6c0ebe54e6e1395ddfc3a105
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2017-11-28 12:33:33 +0000

    retroarch-assets: fix build
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

--- package/retroarch-assets/retroarch-assets.mk
+++ package/retroarch-assets/retroarch-assets.mk
@@ -5,13 +5,14 @@
 ################################################################################
 RETROARCH_ASSETS_VERSION = 80dc7b60a5b5e7b976e005583d6c5569534e9869
 RETROARCH_ASSETS_SITE = $(call github,deltabeard,retroarch-assets,$(RETROARCH_ASSETS_VERSION))
+RETROARCH_MAKE_COMMAND = install
+
+ifeq ($(BR2_PACKAGE_RETROARCH_ASSETS_XMB_ONLY),y)
+RETROARCH_MAKE_COMMAND = install-xmb-monochrome
+endif
 
 define RETROARCH_ASSETS_INSTALL_TARGET_CMDS
-ifeq($(BR2_PACKAGE_RETROARCH_ASSETS_XMB_ONLY),y)
-	$(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install-xmb-monochrome
-else
-	$(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
-endif
+$(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) $(RETROARCH_MAKE_COMMAND)
 endef
 
 $(eval $(generic-package))
